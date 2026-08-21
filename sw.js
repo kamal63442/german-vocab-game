27873660f1879079de98b6f5c63af9c7e92beec1
kamal63442 <kamal63442@users.noreply.github.com>
2026-08-21 16:26:44 +0530

    Fix PWA install: absolute manifest paths, add id field, bump SW cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "uebungsheft-v3";
+const CACHE_NAME = "uebungsheft-v4";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (event) => {
